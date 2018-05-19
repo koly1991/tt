7664665f0d011bd18aa70916a7b0640bf2740244
x <victor.hdez.barrios@gmail.com>
2018-05-20 00:16:59 +0200

    changed start script

--- s.sh
+++ s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ ! -z $1 ] && [ ! -z $2 ] && [ ! -z $3 ] ; then
 	dir=$(mktemp -d)
+	olddir=$HOME
 	HOME=$dir
 	git config user.email $1
 	git config user.name $2
@@ -10,6 +11,7 @@ if [ ! -z $1 ] && [ ! -z $2 ] && [ ! -z $3 ] ; then
 	if ! chromium $(cat pdfs.list) /tmp/main.pdf &> /dev/null ; then
 		chromium-browser $(cat pdfs.list) /tmp/main.pdf &> /dev/null
 	fi &
+	HOME=$olddir
 	screen 
 	rm -rf $dir
 fi
