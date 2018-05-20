a2f5990f5d1eb51f9ab8cb25f66f7104e7723f39
x <victor.hdez.barrios@gmail.com>
2018-05-20 22:57:40 +0200

    autocommit of Sun May 20 22:57:40 CEST 2018

--- search.sh
+++ search.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 if [ ! -z $1 ]; then
-	for i in $(grep -rHal "$1" | shuf); do 
+	for i in $(grep -rHal "$1" | shuf | grep -v swp); do 
 		echo $i
 		cat $i | grep --color -B 3 -A 3 "$1"
 		read
