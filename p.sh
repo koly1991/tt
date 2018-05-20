62b8964b1b6caa834bcc235b7d201f9d5bbf0ccc
victor <victor.hdez.barrios@gmail.com>
2018-05-20 03:55:14 +0200

    autocommit of Sun May 20 03:55:14 CEST 2018

--- p.sh
+++ p.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/zsh
 dir=$(mktemp -d)
-cp pdfs.list $dir
-cd $dir
 for i in $(cat pdfs.list); do
 	wget $i &
 done
 wait
-(zathura * /tmp/main.pdf
-cd -
-rm -rf $dir) &
+zathura * /tmp/main.pdf &> /dev/null &
