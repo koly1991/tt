ac293a256adb3415d4a8cc140b33e55e74138643
x <victor.hdez.barrios@gmail.com>
2018-05-21 12:15:29 +0200

    autocommit of Mon May 21 12:15:29 CEST 2018

--- search.sh
+++ search.sh
@@ -3,10 +3,11 @@ if [ ! -z $1 ]; then
 	for i in $(grep -rHal "$1" | shuf | grep -v swp | grep -v git | grep -v swo); do 
 		echo $i
 		cat $i | grep --color -B 3 -A 3 "$1"
-		read
-		if [ $2 = "edit" ]; then
+		read -sk key
+		if [ $key = "e" ]; then
 			vim $i
+		elif [ $key = "j" ]; then
+			break
 		fi
-		clear
        	done
 fi
